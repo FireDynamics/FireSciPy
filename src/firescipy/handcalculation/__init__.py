40f1d6a25dd033347997ee05bc9b9494701f5062
TristanHehnen <tristan.hehnen@cern.ch>
2025-11-05 10:46:00 +0100

    Relicense to MPL-2.0; bump version to 0.0.6

--- src/firescipy/handcalculation/__init__.py
+++ src/firescipy/handcalculation/__init__.py
@@ -1,1 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
 from .design_fires import alpha_t_squared, simple_design_fire
